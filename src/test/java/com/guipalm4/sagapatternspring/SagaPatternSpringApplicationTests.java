b1293db68d224dfbfca1e391827fc619ab22981c
guipalm4 <gomes.lmc@gmail.com>
2025-12-01 10:15:51 -0300

    test: add Spring Boot application context test
    - Add SagaPatternSpringApplicationTests
    - Verify application context loads successfully

---
+++ src/test/java/com/guipalm4/sagapatternspring/SagaPatternSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guipalm4.sagapatternspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaPatternSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
